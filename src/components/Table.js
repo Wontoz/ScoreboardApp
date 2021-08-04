e1ce1f8acd4d5710a2cbc0bd192b44fa6cd2069d
Wontoz <williamisaksson@hotmail.com>
2021-08-04 21:07:38 +0200

    Users can now increase and decrease win/loss values in existing table rows

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,53 +1,16 @@
 import React from 'react';
 import { Table, Button, Row, Col, Modal, Input} from 'antd';
 
-class ConfirmModal extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      visible: false
-    }
-  }
-
-  showModal = () => {
-    this.setState({
-      visible: true,
-    });
-  };
-
-  hideModal = () => {
-    this.setState({
-      visible: false,
-    });
-  };
-
-  render() {
-    return(
-      <>
-      <p onClick={this.showModal}>Add {this.props.type}</p>
-      <Modal
-      title="Alter Score"
-      visible={this.state.visible}
-      onOk={this.hideModal}
-      onCancel={this.hideModal}
-      okText="Yes"
-      cancelText="No">
-        Are you sure you want to adjust the score of {this.props.row}?
-      </Modal>
-      </>
-    )
-  }
-}
-
 class TableBody extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      visible: false,
       players : [
-        { key: 1, participant: 'Player 1', wins: 1337, losses: 0},
-        { key: 2, participant: 'Player 2', wins: 1337, losses: 0},
-        { key: 3, participant: 'Player 3', wins: 1337, losses: 0},
+        { key: 0, participant: 'Player 1', wins: 5, losses: 0},
+        { key: 1, participant: 'Player 2', wins: 4, losses: 0},
+        { key: 2, participant: 'Player 3', wins: 3, losses: 0},
       ],
       columns: [
         {
@@ -69,16 +32,20 @@ class TableBody extends React.Component {
           title: 'Add Win',
           dataIndex: 'addWin',
           key: 'addWin',
-          render: (key, record,) => (
-            <ConfirmModal row={record.participant} type="win" onClick={this.handleDeleteRow.bind(this, record.key)}></ConfirmModal>
+          render: (key, record) => (
+            <p onClick ={this.handleWinIncrease.bind(this, record.key)}>
+              Add win to player {record.key}
+            </p>
           )
         },
         {
           title: 'Add loss',
           dataIndex: 'addLoss',
           key: 'addLoss',
-          render: (participant, record,) => (
-            <ConfirmModal row={record.participant} type="loss"></ConfirmModal>
+          render: (key, record,) => (
+            <p onClick ={this.handleLossIncrease.bind(this, record.key)}>
+              Add loss to player {record.key}
+            </p>
           )
         },
         {
@@ -95,11 +62,19 @@ class TableBody extends React.Component {
       playerName: ('')
     }
   }
-
-  handleWinIncrease(key) {
-    const {players} = this.state;
-    alert(players[key]);
+  
+  handleWinIncrease(playerKey) {
+    const newArr = this.state.players.slice()
+    newArr[playerKey].wins++;
+    this.setState({players: newArr})
   }
+
+  handleLossIncrease(playerKey) {
+    const newArr = this.state.players.slice()
+    newArr[playerKey].losses++;
+    this.setState({players: newArr})
+  }
+  
 
   handleDeleteRow(key) {
     const {players} = this.state;
@@ -111,17 +86,17 @@ class TableBody extends React.Component {
       this.setState({isModalVisible: true})
   }
 
-hideModal(){
-    this.setState({isModalVisible: false})
-} 
-changePlayerName(e) {
-    this.setState({playerName: e.target.value})
-}
+  hideModal(){
+      this.setState({isModalVisible: false})
+  } 
+  changePlayerName(e) {
+      this.setState({playerName: e.target.value})
+  }
 
   //Denna ska parent node ha
-  addPlayer(name) {
+  addPlayer() {
     const {players} = this.state;
-    const newPlayerStat = [{ key: (players.length + 1), participant: this.state.playerName, wins: 0, losses: 0}]
+    const newPlayerStat = [{ key: (players.length), participant: this.state.playerName, wins: 0, losses: 0}]
     const joined = this.state.players.concat(newPlayerStat);
     this.setState({players: joined})
     this.hideModal();
